ab0362c504dfb5afa014ca40ae5e1627f262e5a2
Alvin Raygon <alvinraygon@yahoo.com>
2021-02-17 20:57:22 +0800

    Initial Commit

---
+++ SampleIOS/SampleIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleIOSApp.swift
+//  SampleIOS
+//
+//  Created by Alvin Raygon on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
