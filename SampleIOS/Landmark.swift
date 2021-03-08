5718b760cb7bd5b7fe2a224c46bcbac7f4acebf0
Alvin Raygon <alvinraygon@yahoo.com>
2021-03-08 10:12:21 +0800

    Added landmark.json, landmark model, and images

---
+++ SampleIOS/Landmark.swift
@@ -0,0 +1,38 @@
+//
+//  Landmark.swift
+//  SampleIOS
+//
+//  Created by Alvin Raygon on 3/8/21.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable,Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    // You make the property private because users of the Landmarks structure care only about the image itself.
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    // Compute a locationCoordinate property that’s useful for interacting with the MapKit framework.
+    var locationCoordinate: CLLocationCoordinate2D{
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    // Add a coordinates property to the structure using a nested Coordinates type that reflects the storage in the JSON data structure.
+    struct Coordinates: Hashable,Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
