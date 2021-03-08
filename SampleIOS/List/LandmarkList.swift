2545885c3a7978d4549e35b236c4e41b9a9823b1
Alvin Raygon <alvinraygon@yahoo.com>
2021-03-08 15:57:01 +0800

    Setup Navigation between list and detail

--- SampleIOS/List/LandmarkList.swift
+++ SampleIOS/List/LandmarkList.swift
@@ -9,11 +9,18 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        /*
-         Lists work with identifiable data. You can make your data identifiable in one of two ways: by passing along with your data a key path to a property that uniquely identifies each element, or by making your data type conform to the Identifiable protocol.
-         */
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
+        // Embed the dynamically generated list of landmarks in a NavigationView.
+        NavigationView {
+            /*
+             Lists work with identifiable data. You can make your data identifiable in one of two ways: by passing along with your data a key path to a property that uniquely identifies each element, or by making your data type conform to the Identifiable protocol.
+             */
+            List(landmarks) { landmark in
+                // Inside the list’s closure, wrap the returned row in a NavigationLink, specifying the LandmarkDetail view as the destination.
+                NavigationLink(destination: LandmarkDetail()){
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
         }
     }
 }
