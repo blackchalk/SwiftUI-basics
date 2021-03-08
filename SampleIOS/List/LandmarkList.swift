ae9a6f9d76476fbb9ea139d99fe67262a48d587a
Alvin Raygon <alvinraygon@yahoo.com>
2021-03-08 17:56:10 +0800

    Filter the list view

--- SampleIOS/List/LandmarkList.swift
+++ SampleIOS/List/LandmarkList.swift
@@ -8,13 +8,20 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = true
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
     var body: some View {
         // Embed the dynamically generated list of landmarks in a NavigationView.
         NavigationView {
             /*
              Lists work with identifiable data. You can make your data identifiable in one of two ways: by passing along with your data a key path to a property that uniquely identifies each element, or by making your data type conform to the Identifiable protocol.
              */
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 // Inside the list’s closure, wrap the returned row in a NavigationLink, specifying the LandmarkDetail view as the destination.
                 NavigationLink(destination: LandmarkDetail(landmark: landmark)){
                     LandmarkRow(landmark: landmark)
@@ -27,13 +34,13 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+//        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
         LandmarkList()
-            // By default, previews render at the size of the device in the active scheme. You can change
-            // the preview device by calling the previewDevice(_:) modifier method.
-            .previewDevice(PreviewDevice(rawValue: deviceName))
-            // Use the previewDisplayName(_:) modifier to add the device names as labels for the previews.
-            .previewDisplayName(deviceName)
-        }
+//            // By default, previews render at the size of the device in the active scheme. You can change
+//            // the preview device by calling the previewDevice(_:) modifier method.
+//            .previewDevice(PreviewDevice(rawValue: deviceName))
+//            // Use the previewDisplayName(_:) modifier to add the device names as labels for the previews.
+//            .previewDisplayName(deviceName)
+//        }
     }
 }
