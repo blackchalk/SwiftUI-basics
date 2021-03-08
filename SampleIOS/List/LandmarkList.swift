a954a7d316c9848e994a4e48015c39c6236bcb26
Alvin Raygon <alvinraygon@yahoo.com>
2021-03-08 18:03:28 +0800

    Add a control to toggle the state

--- SampleIOS/List/LandmarkList.swift
+++ SampleIOS/List/LandmarkList.swift
@@ -8,24 +8,36 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    @State private var showFavoritesOnly = true
+    @State private var showFavoritesOnly = false
     
     var filteredLandmarks: [Landmark] {
         landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
+    
     var body: some View {
         // Embed the dynamically generated list of landmarks in a NavigationView.
         NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
             /*
-             Lists work with identifiable data. You can make your data identifiable in one of two ways: by passing along with your data a key path to a property that uniquely identifies each element, or by making your data type conform to the Identifiable protocol.
+             Lists work with identifiable data. You can make your data identifiable in one of two ways:
+                 by passing along with your data a key path to a property that uniquely identifies each element,
+                 or by making your data type conform to the Identifiable protocol.
+                 
+             To combine static and dynamic views in a list, or to combine two or more different groups of dynamic views,
+                 use the ForEach type instead of passing your collection of data to List.
+
              */
-            List(filteredLandmarks) { landmark in
+            ForEach(filteredLandmarks) { landmark in
                 // Inside the list’s closure, wrap the returned row in a NavigationLink, specifying the LandmarkDetail view as the destination.
                 NavigationLink(destination: LandmarkDetail(landmark: landmark)){
                     LandmarkRow(landmark: landmark)
                 }
+            }
             }
             .navigationTitle("Landmarks")
         }
